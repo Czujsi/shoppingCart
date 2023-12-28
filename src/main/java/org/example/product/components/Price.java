dcdfd81d2d1d7d94bb8c2bd36ea13eae1eaf8bdf
Czujsi <oskarbanaszczuk@gmail.com>
2023-12-28 20:39:59 +0100

    clearing whitespaces

--- src/main/java/org/example/product/components/Price.java
+++ src/main/java/org/example/product/components/Price.java
@@ -7,12 +7,9 @@ import org.example.currency_exchange_money.Money;
 @EqualsAndHashCode
 @ToString
 public class Price {
-
     public Money productPrice;
 
     public Price(Money productPrice) {
         this.productPrice = productPrice;
     }
-
-
 }
