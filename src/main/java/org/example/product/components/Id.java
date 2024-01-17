a7774e3b5198481bb69235cc07990c1ae9d18f9a
Czujsi <oskarbanaszczuk@gmail.com>
2024-01-17 17:00:05 +0100

    adding id class with creatingIdMethod

--- src/main/java/org/example/product/components/Id.java
+++ src/main/java/org/example/product/components/Id.java
@@ -1,5 +1,18 @@
 package org.example.product.components;
 
-public class Id {
+import lombok.EqualsAndHashCode;
 
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+@EqualsAndHashCode
+public class Id {
+    private static final AtomicLong atomicCounter = new AtomicLong();
+
+    public static String createId() {
+
+        String currentCounter = String.valueOf(atomicCounter.getAndIncrement());
+        String uniqueId = UUID.randomUUID().toString();
+
+        return uniqueId + "-" + currentCounter;
+    }
 }
