135852a5267b77d7465e6bddd4368f88198c4a21
Czujsi <oskarbanaszczuk@gmail.com>
2024-02-20 17:20:22 +0100

    adding AccountTest class and test to it

--- src/test/java/org/example/account/AccountTest.java
+++ src/test/java/org/example/account/AccountTest.java
@@ -2,10 +2,14 @@ package org.example.account;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class AccountTest {
     @Test
     void creatingAccountTest() {
         Account account = new Account(new UserLogin("tomek1231"), new UserPassword("password123"), new UserName("Tomasz"));
 
+        assertThat(account.getUserId()).isEqualTo(account.getUserId());
+        assertThat(account.getUserName()).isEqualTo(new UserName("Tomasz"));
     }
 }
