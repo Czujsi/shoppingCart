48b73f29a2a7e2412d50d20e5ac59b7bf81d28fa
Czujsi <oskarbanaszczuk@gmail.com>
2024-02-20 17:23:58 +0100

    changing access modifiers in static variables

--- src/test/java/org/example/product/repository/ProductRepositoryFileImplTest.java
+++ src/test/java/org/example/product/repository/ProductRepositoryFileImplTest.java
@@ -19,25 +19,25 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class ProductRepositoryFileImplTest {
-    static ProductDefinition EXAMPLE_PRODUCT_1 = new ProductDefinition(new Name("Banana1")
+    private static final ProductDefinition EXAMPLE_PRODUCT_1 = new ProductDefinition(new Name("Banana1")
             , new Price(Money.of(BigDecimal.valueOf(2.65), Currency.PLN))
             , new CreationDate(LocalDate.now())
             , new ProductId("2f5cd2c6-a71c-4c57-a06a-bf4c422e9a31"));
 
-    static ProductDefinition EXAMPLE_PRODUCT_2 = new ProductDefinition(new Name("Banana2")
+    private static final ProductDefinition EXAMPLE_PRODUCT_2 = new ProductDefinition(new Name("Banana2")
             , new Price(Money.of(BigDecimal.valueOf(2.65), Currency.PLN))
             , new CreationDate(LocalDate.now())
             , new ProductId("2f5cd2c6-a71c-4c57-a06a-bf4c422e9a32"));
-    static ProductDefinition EXAMPLE_PRODUCT_3 = new ProductDefinition(new Name("Banana3")
+    private static final ProductDefinition EXAMPLE_PRODUCT_3 = new ProductDefinition(new Name("Banana3")
             , new Price(Money.of(BigDecimal.valueOf(2.65), Currency.PLN))
             , new CreationDate(LocalDate.now())
             , new ProductId("2f5cd2c6-a71c-4c57-a06a-bf4c422e9a33"));
-    static ProductDefinition EXAMPLE_PRODUCT_4 = new ProductDefinition(new Name("Banana4")
+    private static final ProductDefinition EXAMPLE_PRODUCT_4 = new ProductDefinition(new Name("Banana4")
             , new Price(Money.of(BigDecimal.valueOf(2.65), Currency.PLN))
             , new CreationDate(LocalDate.now())
             , new ProductId("2f5cd2c6-a71c-4c57-a06a-bf4c422e9a34"));
 
-    static ProductDefinition EXAMPLE_PRODUCT_5 = new ProductDefinition(new Name("Banana5")
+    private static final ProductDefinition EXAMPLE_PRODUCT_5 = new ProductDefinition(new Name("Banana5")
             , new Price(Money.of(BigDecimal.valueOf(2.65), Currency.PLN))
             , new CreationDate(LocalDate.now())
             , new ProductId("2f5cd2c6-a71c-4c57-a06a-bf4c422e9a35"));
